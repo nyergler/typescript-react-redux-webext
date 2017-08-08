e0d9d1acd9e0faa77471367ac70cbd5babd77e64
Nathan Yergler <nathan@yergler.net>
2017-08-08 14:34:16 -0700

    Minor reorganization

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,39 +1,10 @@
-import * as React from "react";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import ActiveTabPopup from './components/activeTab';
+// import './index.css';
 
-import Nested from './nested-component';
 
-interface PopupProps { };
-interface PopupState { activeTab: browser.tabs.Tab | null };
-
-export default class Popup extends React.Component {
-
-  state : PopupState;
-
-  constructor(props: PopupProps) {
-    super(props);
-    this.state = {activeTab: null};
-  }
-
-  componentDidMount() {
-    // Get the active tab and store it in component state.
-    browser.tabs.query({active: true}).then(
-      tabs => this.setState({activeTab: tabs[0]})
-    );
-  }
-
-  render() {
-    const {activeTab} = this.state;
-    return (
-      <div>
-        <h1>React Component</h1>
-        <p>
-          This is an example of a popup UI in React.
-        </p>
-        <p>
-          Active tab: {activeTab ? activeTab.url : '[waiting for result]'}
-        </p>
-        <Nested />
-      </div>
-    );
-  }
-}
+ReactDOM.render(
+  <ActiveTabPopup/>,
+  document.getElementById('root') as HTMLElement
+);
